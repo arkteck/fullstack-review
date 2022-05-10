e3669fab3aa3216ef027bef66cbdbcd813eb6da4
arkteck <arkteck@gmail.com>
2022-05-10 10:02:54 -0400

    Removed comments

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
   console.log('get repos of ', req.body.username);
   github.getReposByUsername(req.body.username)
     .then(data => {
@@ -27,12 +23,9 @@ app.post('/repos', function (req, res) {
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
 
   return database.retrieve()
     .then(data => {
-      // console.log('RETRIEVED DATA', data);
       res.send(data);
       res.end();
     })
