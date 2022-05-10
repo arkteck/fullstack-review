ab876d376aaea86557fc4d6e3850884bec5f4bf3
arkteck <arkteck@gmail.com>
2022-05-10 16:17:37 -0400

    Refactoring

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,6 +15,7 @@ class App extends React.Component {
       order: -1,
     }
     this.render = this.render.bind(this);
+    this.refresh = this.refresh.bind(this);
     this.search = this.search.bind(this);
     this.handleClick = this.handleClick.bind(this);
 
@@ -35,24 +36,27 @@ class App extends React.Component {
           alert(`Encountered errors when adding repos from ${username}. See console for more details.`)
           console.log(data);
         }
-        this.render();
+        this.refresh();
       }
     } )
   }
 
-  render () {
+  refresh () {
     $.ajax(`/repos/${this.state.sortBy}@${this.state.order.toString()}`,
     {
       method: 'GET',
       dataType: 'json',
       success: data => {
         this.setState({users: data[0], count: data[1], repos: data[2]});
+        this.render();
       },
       error: (jqxhr, textStatus, errorThrown) => {
-        console.log('render ajax error', textStatus)
+        console.log('refresh ajax error', textStatus)
       }
     })
+  }
 
+  render () {
     return (<div>
       <h1>Github Fetcher</h1>
       <RepoList count={this.state.count} repos={this.state.repos} handleClick={this.handleClick} sortBy={this.state.sortBy} order={this.state.order} users={this.state.users}/>
