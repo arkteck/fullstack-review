df141b51b6aa86df2dd5635897cb165819f75cb5
arkteck <arkteck@gmail.com>
2022-05-10 09:29:30 -0400

    More pre-Heroku stuff

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+const Promise = require('bluebird');
+
 let repoSchema = mongoose.Schema({
   github_id: {type: Number, unique: true},
   name: String,
@@ -37,24 +39,14 @@ let save = (data) => {
     m.open_issues = d.open_issues;
     mongoData.push(m);
   })
-  // console.log('database save data', mongoData);
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
 
   return Repo.insertMany(mongoData, {ordered: false});
 }
 
 let retrieve = () => {
-  var count = 0;
 
-  return Repo.estimatedDocumentCount()
-  .then(data => {
-    count = data;
-    return Repo.find({}, null, {sort: {size: -1} , limit: 25});
-  }).then(data => {
-    return [count, data];
-  })
+  const promArr = [Repo.estimatedDocumentCount(), Repo.find({}, null, {sort: {size: -1} , limit: 25})];
+  return Promise.all(promArr);
 
 }
 
